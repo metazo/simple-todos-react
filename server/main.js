9b35166edb52bfcf4c3510959165113a88c06da5
metazo <metaz0412@gmail.com>
2022-09-24 19:08:00 +0200

    intitial commit

--- server/main.js
+++ server/main.js
@@ -1,5 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
+import { Meteor } from "meteor/meteor";
+import { TasksCollection } from "/imports/api/TasksCollection";
+
+const insertTask = (taskText) => TasksCollection.insert({ text: taskText });
+
+Meteor.startup(() => {
+  if (TasksCollection.find().count() === 0) {
+    [
+      "First Task",
+      "Second Task",
+      "Third Task",
+      "Fourth Task",
+      "Fifth Task",
+      "Sixth Task",
+      "Seventh Task",
+    ].forEach(insertTask);
+  }
+});
 
 function insertLink({ title, url }) {
   LinksCollection.insert({title, url, createdAt: new Date()});
